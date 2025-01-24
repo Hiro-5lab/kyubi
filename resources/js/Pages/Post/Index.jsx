e09c77902051e526034b36c9ed7ee25d7536763f
Hiro-5lab <hiromu.50505002@gmail.com>
2025-01-24 01:33:18 +0000

    add Post function

--- resources/js/Pages/Post/Index.jsx
+++ resources/js/Pages/Post/Index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { Link, router } from '@inertiajs/react'
 
 const Index = (props) => {
-    console.log(props);
+    const { posts } = props; 
+    
+    const handleDeletePost = (id) => {
+        router.delete(`/posts/${id}`, {
+            onBefore: () => confirm("本当に削除しますか？"),
+        })
+    }
+
     return (
         <Authenticated user={props.auth.user} header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
@@ -11,13 +19,18 @@ const Index = (props) => {
             }>
             
             <div className="p-12">
+                <Link href="/posts/create">Create</Link>
                 <h1>Blog Name</h1>
-                <div>
-                    <div>
-                        <h2>Title</h2>
-                        <p>This is a simple body.</p>
+                { posts.map((post) => (
+                    <div key={post.id}>
+                        <h2>
+                            <Link href={`/posts/${post.id}`}>{ post.title }</Link>
+                        </h2>
+                        <p>{ post.body }</p>
+                        
+                        <button className="p-1 bg-purple-300 hover:bg-purple-400 rounded-md" onClick={() => handleDeletePost(post.id)}>delete</button>
                     </div>
-                </div>
+                )) }
             </div>
             
         </Authenticated>
